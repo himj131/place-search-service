99f6ac39adc77928ea82d902f7ad20ade8f3dc29
Winter <winter@Winterui-MacBookPro.local>
2022-07-14 20:18:34 +0900

    first commit

---
+++ src/main/java/com/himj/placesearchservice/PlaceSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.himj.placesearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/himj/placesearchservice/PlaceSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himj.placesearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
