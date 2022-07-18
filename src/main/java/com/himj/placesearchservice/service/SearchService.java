f8b6cc84a087353defab2152961aa5cd88e6e2f7
Winter <himj131@gmail.com>
2022-07-19 06:55:16 +0900

    feat: 검색결과 캐싱적용

--- src/main/java/com/himj/placesearchservice/service/SearchService.java
+++ src/main/java/com/himj/placesearchservice/service/SearchService.java
@@ -3,6 +3,7 @@ package com.himj.placesearchservice.service;
 import com.himj.placesearchservice.domain.Events;
 import com.himj.placesearchservice.domain.KeywordSearchEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class SearchService {
     private final NaverSearchService naverSearcheService;
     private final KakaoSearchService kaKaoSearchService;
 
+    @Cacheable(value = "searchedKeywords", key = "#searchRequest")
     public List<String> searchByKeyword(SearchRequest searchRequest) {
         Events.raise(new KeywordSearchEvent(searchRequest.getKeyword()));
         KakaoSearchResponse kakaoRes = kaKaoSearchService.searh(searchRequest);
